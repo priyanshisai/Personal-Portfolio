163c5616bb4ce934d6b690bf8f89a2993a2bbf10
Priyanshi Sai <priyanshisainicky@gmail.com>
2026-06-10 20:10:50 +0530

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+//console.log('Happy developing ✨')
+
+//spacy, scary, military, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("cowboy")
+
+// watermelon, tomato, banana, orange, avocado, blueberry
+favouriteFruit("banana")
+
+// light, dark
+favouriteMode("Dark")
+
+//sharp, soft, round
+favouriteEdgeStyle("sharp ")
